17607e89bf690927dc32caf510dee6d73bf8c3ff
TannerGabriel <gabrieltanner.code@gmail.com>
2024-04-26 17:39:01 +0200

    feat: Change predicted points when the slider changes
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- app/src/main/java/at/aau/serg/fragments/TrickPredictionFragment.kt
+++ app/src/main/java/at/aau/serg/fragments/TrickPredictionFragment.kt
@@ -1,30 +1,57 @@
 package at.aau.serg.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import at.aau.serg.R
+import com.google.android.material.slider.Slider
+
+private const val ARG_ROUND = "round"
 
 class TrickPredictionFragment : Fragment() {
+    private var round: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            round = it.getInt(ARG_ROUND)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trick_prediction, container, false)
+        val view = inflater.inflate(R.layout.fragment_trick_prediction, container, false)
+
+        val trickPredictionSlider: Slider =  view.findViewById(R.id.trickPredictionSlider)
+        val scoreTextView: TextView = view.findViewById(R.id.predictionScoreTV)
+
+        trickPredictionSlider.addOnChangeListener { _, value, _ ->
+            val predictionPoints = "${calculateReachablePoints(value.toInt())} Points"
+            scoreTextView.text = predictionPoints
+        }
+
+        trickPredictionSlider.valueTo = round.toFloat()
+
+        return view
+    }
+
+    private fun calculateReachablePoints(prediction: Int): Int {
+        return prediction * 20 + 20;
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(round: Int) =
             TrickPredictionFragment().apply {
-                arguments = Bundle().apply {}
+                arguments = Bundle().apply {
+                    putInt(ARG_ROUND, round)
+                }
             }
     }
 }
