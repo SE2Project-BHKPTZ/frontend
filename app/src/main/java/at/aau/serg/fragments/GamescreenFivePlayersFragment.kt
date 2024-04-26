8b2ffe71f59b7fb230f0873c26961a5842d6404a
Benjamin Hacksteiner <benjaminhac@edu.aau.at>
2024-04-26 18:01:43 +0200

    feat: implement gamescreen fragments for different player counts

---
+++ app/src/main/java/at/aau/serg/fragments/GamescreenFivePlayersFragment.kt
@@ -0,0 +1,34 @@
+package at.aau.serg.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import at.aau.serg.R
+
+class GamescreenFivePlayersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gamescreen_five_players, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            GamescreenFivePlayersFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
---
+++ app/src/main/java/at/aau/serg/fragments/GamescreenSixPlayersFragment.kt
@@ -0,0 +1,34 @@
+package at.aau.serg.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import at.aau.serg.R
+
+class GamescreenSixPlayersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gamescreen_six_players, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            GamescreenSixPlayersFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
