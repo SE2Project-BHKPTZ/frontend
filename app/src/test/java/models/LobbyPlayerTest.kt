049fb0807a08d9dda209ec43b4be262736c0f575
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-04-28 17:11:20 +0200

    fix: fixed test for LobbyPlayer

--- app/src/test/java/models/LobbyPlayerTest.kt
+++ app/src/test/java/models/LobbyPlayerTest.kt
@@ -2,28 +2,29 @@ package models
 
 
 import at.aau.serg.models.LobbyPlayer
+import at.aau.serg.models.Visibilities
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class LobbyPlayerTest {
     @Test
     fun `LobbyPlayer instantiation`() {
-        val lobbyPlayer = LobbyPlayer("TestPlayerUUID","TestPlayer" ,0)
+        val lobbyPlayer = LobbyPlayer("TestPlayerUUID","TestPlayer" ,Visibilities.VISIBLE)
         assertEquals("TestPlayerUUID", lobbyPlayer.uuid)
         assertEquals("TestPlayer", lobbyPlayer.name)
-        assertEquals(0, lobbyPlayer.isVisible)
+        assertEquals(Visibilities.VISIBLE, lobbyPlayer.isVisible)
     }
 
     @Test
     fun `LobbyPlayer setter`() {
-        val lobbyPlayer = LobbyPlayer("TestPlayerUUID","TestPlayer" ,0)
+        val lobbyPlayer = LobbyPlayer("TestPlayerUUID","TestPlayer" ,Visibilities.VISIBLE)
 
         lobbyPlayer.uuid = "otherUUID"
         lobbyPlayer.name = "otherName"
-        lobbyPlayer.isVisible = 4
+        lobbyPlayer.isVisible = Visibilities.INVISIBLE
 
         assertEquals("otherUUID", lobbyPlayer.uuid)
         assertEquals("otherName", lobbyPlayer.name)
-        assertEquals(4, lobbyPlayer.isVisible)
+        assertEquals(Visibilities.INVISIBLE, lobbyPlayer.isVisible)
     }
 }
