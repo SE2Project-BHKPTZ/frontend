661fc9f106d8e08364dc777ef0630480178ae253
Benjamin Hacksteiner <benjaminhac@edu.aau.at>
2024-05-10 09:18:04 +0200

    feat: add socket endpoint to receive updated score

--- app/src/main/java/at/aau/serg/activities/GameScreenActivity.kt
+++ app/src/main/java/at/aau/serg/activities/GameScreenActivity.kt
@@ -2,7 +2,9 @@ package at.aau.serg.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +12,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import at.aau.serg.R
-import at.aau.serg.fragments.TrickPredictionFragment
-import at.aau.serg.fragments.TrickPredictionViewModel
 import at.aau.serg.fragments.GameScreenFivePlayersFragment
 import at.aau.serg.fragments.GameScreenFourPlayersFragment
 import at.aau.serg.fragments.GameScreenSixPlayersFragment
 import at.aau.serg.fragments.GameScreenThreePlayersFragment
+import at.aau.serg.fragments.TrickPredictionFragment
+import at.aau.serg.fragments.TrickPredictionViewModel
+import at.aau.serg.network.SocketHandler
+import org.json.JSONObject
 
 class GameScreenActivity : AppCompatActivity() {
     private val trickViewModel: TrickPredictionViewModel by viewModels()
@@ -42,6 +46,7 @@ class GameScreenActivity : AppCompatActivity() {
             transaction.commit()
         }
 
+        SocketHandler.on("game:updateUserScore", ::updateUserScore)
     }
 
     // only for showing the different GameScreens now
@@ -72,5 +77,18 @@ class GameScreenActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainerViewGame, newFragment)
                 .commit()
         }
+    }
+
+    private fun updateUserScore(socketResponse: Array<Any>) {
+        Log.d("Socket", "Received updateUserScore event")
+
+        val playerData = (socketResponse[0] as JSONObject)
+        val playerUUID = playerData.getString("playerUUID")
+        val playerPoints = playerData.getInt("points")
+        Log.d("Game", "$playerUUID has $playerPoints points")
+
+        val tvPlayer1Points = findViewById<TextView>(R.id.tvPlayer1Points)
+        tvPlayer1Points?.text = playerPoints.toString()
+
     }
 }
