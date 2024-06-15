8feb67894555f448318296a3bebce2053e6e0631
Alexander Pichler <alexander.pichler.fi@gmail.com>
2024-06-15 09:47:23 +0200

    feat: polling for updates in JoinLobby activity

--- app/src/main/java/at/aau/serg/activities/JoinLobbyActivity.kt
+++ app/src/main/java/at/aau/serg/activities/JoinLobbyActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.InputType
 import android.util.Log
 import android.view.View
@@ -35,6 +37,8 @@ class JoinLobbyActivity : AppCompatActivity() {
     private val lobbies: MutableList<JoinLobbyLobby> = mutableListOf()
     private lateinit var adapter: JoinLobbyLobbiesAdapter
     private var lobbyToJoin = LobbyJoin("")
+    private val handler = Handler(Looper.getMainLooper())
+    private val pollingInterval: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,11 +56,22 @@ class JoinLobbyActivity : AppCompatActivity() {
         recyclerView.setLayoutManager(LinearLayoutManager(this))
         recyclerView.setAdapter(adapter)
 
-        HttpClient.get(
-            "lobbys",
-            StoreToken(this).getAccessToken(),
-            CallbackCreator().createCallback(::onFailureLobby, ::onSuccessGetLobbies)
-        )
+        startPolling()
+    }
+
+    private fun startPolling() {
+        handler.post(pollingRunnable)
+    }
+
+    private val pollingRunnable = object : Runnable {
+        override fun run() {
+            HttpClient.get(
+                "lobbys",
+                StoreToken(this@JoinLobbyActivity).getAccessToken(),
+                CallbackCreator().createCallback(::onFailureLobby, ::onSuccessGetLobbies)
+            )
+            handler.postDelayed(this, pollingInterval)
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -92,7 +107,6 @@ class JoinLobbyActivity : AppCompatActivity() {
             Log.d("LO", lobbies.toString())
         }
     }
-
 
     fun btnJoinCode(view: View) {
         createJoinLobbyPrompt(null)
@@ -165,5 +179,10 @@ class JoinLobbyActivity : AppCompatActivity() {
 
     fun btnBack(view: View) {
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(pollingRunnable)
     }
 }
